e2eb9eaabdc3ca3fd9d74a861bfbc679dc91ade8
yatou666 <1904692862@qq.com>
2018-04-04 00:21:45 +0800

    1

--- js/index.js
+++ js/index.js
@@ -22,10 +22,15 @@
 		}
 	});
 
-	
+	let flag=true;
 	let n=0; 
 	let t=setInterval(move,3000);
 	function move(){
+		// if(flag){
+
+		// }else{
+
+		// }
 		n++;
 		if(n===imgs.length){
 			n=0;
@@ -102,48 +107,78 @@
 	}
 }
 
-	//leftBar开始
-{
-	let tips=document.querySelectorAll(".tip");
-	let containers=document.querySelectorAll(".container");
+	//原生jsleftBar开始
+// {
+// 	let tips=document.querySelectorAll(".tip");
+// 	let containers=document.querySelectorAll(".container");
 
-	tips.forEach(function(ele,index){
-		ele.onclick=function(){
-			let ot=containers[index].offsetTop-50;
-			let now=document.documentElement.scrollTop;
-			let speed=(ot-now)/8;
-			let time=0;
-			let t=setInterval(function(){
-				time+=25;
-				now+=speed;
-				if(time===200){
-					clearInterval(t);
-				}
-				document.documentElement.scrollTop=ot;
-			},25)
+// 	tips.forEach(function(ele,index){
+// 		ele.onclick=function(){
+// 			let ot=containers[index].offsetTop-50;
+// 			let now=document.documentElement.scrollTop;
+// 			let speed=(ot-now)/8;
+// 			let time=0;
+// 			let t=setInterval(function(){
+// 				time+=25;
+// 				now+=speed;
+// 				if(time===200){
+// 					clearInterval(t);
+// 				}
+// 				document.documentElement.scrollTop=ot;
+// 			},25)
 			
-		}
-	});
+// 		}
+// 	});
 
-	window.addEventListener("scroll",function(){
-		let st=document.documentElement.scrollTop;
-		let obj=tips[0];
-		for(let i=0; i<containers.length;i++){
-			if(st>containers[i].offsetTop-50){
-				for(let i=0;i<tips.length;i++){
-					 tips[i].classList.remove("lfactive");
-				}
-				 tips[i].classList.add("lfactive");
-				// obj.classList.remove("lfactive");
-				// tips[i].classList.add("lfactive");
-				// obj=tips[i];
+// 	window.addEventListener("scroll",function(){
+// 		let st=document.documentElement.scrollTop;
+// 		let obj=tips[0];
+// 		for(let i=0; i<containers.length;i++){
+// 			if(st>containers[i].offsetTop-50){
+// 				for(let i=0;i<tips.length;i++){
+// 					 tips[i].classList.remove("lfactive");
+// 				}
+// 				 tips[i].classList.add("lfactive");
+// 				// obj.classList.remove("lfactive");
+// 				// tips[i].classList.add("lfactive");
+// 				// obj=tips[i];
 
-			}
-		}
-	});
+// 			}
+// 		}
+// 	});
 
 	
+// }
+
+//jquery实现左侧跳楼
+{
+	$(".tip").click(function () {
+
+	    let index=$(this).index(".tip");
+	    console.log(index);
+	    let ot=$(".container").eq(index).offset().top-50;
+	    console.log(ot);
+	    $("html,body").animate({scrollTop:ot},200)
+	})
+
+	$(window).scroll(function () {
+	    let st= $(window).scrollTop();
+	    $(".container").each(function (index,ele) {
+	        if(st>$(this).offset().top-150){
+	            $(".tip").removeClass("lfactive")
+	                .eq(index).addClass("lfactive");
+	        }
+	    })
+	})
 }
+//实现totop
+{
+	$("#db").click(function () {
+
+   	 $("html,body").animate({scrollTop:0},500)
+	})
+}
+
 // 大聚会部分   无缝轮播
 {
 	const jhprev=document.querySelector(".jhbtnleft");
@@ -203,14 +238,11 @@
     		 m--;
 	        phinner.style.transition="all .5s";
 	        phinner.style.marginLeft = -390 * m + "px";
-	        for(let i=0;i<phbtns.length;i++){
-	            phbtns[i].classList.remove("phactive");
-	            phbtns[i-1].classList.add("phactive");
-	        }
+	        // for(let i=0;i<phbtns.length;i++){
+	        //     phbtns[i].classList.remove("phactive");
+	        //     phbtns[i-1].classList.add("phactive");
+	        // }
     	}
-       
-
-
     }
 
     phrightbtn.onclick = function () {
@@ -228,7 +260,7 @@
     	flag=true;
         if(m===4){
             phinner.style.transition="none";
-            phinner.style.marginLeft=-390+"px";
+            phinner.style.marginLeft=-390+"px";            
             m=1;
         }
         if(m===0){
@@ -236,6 +268,7 @@
             phinner.style.marginLeft=-1170+"px";
             m=3;
         }
+      
     })
 
     phbtns.forEach(function(ele,index){
@@ -271,10 +304,17 @@ $(".topxiala").each(function (index,ele) {
 //右侧导航
 $(".message").each(function (index,ele) {
     $(this).mouseenter(function () {
-        $(".rignthide").eq(index).css("display","block");
-        // $(".rignthide").eq(index).css("left","-150");
+        //$(".rignthide").eq(index).css("display","block");
+        $(".rignthide").eq(index).animate({opacity:1},500);
+        $(".rignthide").eq(index).animate({width:"70px"},400);
+        $(".rignthide").eq(index).animate({left:"-70px"},300);
     })
     $(this).mouseleave(function () {
-        $(".rignthide").eq(index).css("display","none");
+        $(".rignthide").eq(index).animate({opacity:"0"},500);
+        $(".rignthide").eq(index).animate({width:"0px"},400);
+        $(".rignthide").eq(index).animate({left:"35px"},300);
     })
 })
+   	
+
+	
